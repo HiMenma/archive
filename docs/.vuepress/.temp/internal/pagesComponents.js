dc70250d2432b3a0fcc062816e790916d7075a23
Yang <40138451+HiMenma@users.noreply.github.com>
2023-06-05 21:13:29 +0800

    init

---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,10 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /
+  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/index.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/404.html.vue")),
+  // path: /.~README.html
+  "v-21564982": defineAsyncComponent(() => import(/* webpackChunkName: "v-21564982" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/.~README.html.vue")),
+}
