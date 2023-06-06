b9489187165778399de9664b7e4af6650363c6b6
Yang <40138451+HiMenma@users.noreply.github.com>
2023-06-06 10:38:57 +0800

    update
    update .gitignore

--- docs/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,8 +0,0 @@
-export const pagesData = {
-  // path: /
-  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-  // path: /.~README.html
-  "v-21564982": () => import(/* webpackChunkName: "v-21564982" */"C:/Users/16229.LAPTOP-OAGS3O3B/Documents/GitHub/archive/docs/.vuepress/.temp/pages/.~README.html.js").then(({ data }) => data),
-}
